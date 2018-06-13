a07576eea2b5c3af3ee333c6b69d3f0ac38727a2
Brice Tessier Huort <tessierhuort@gmail.com>
2018-06-13 15:37:29 +0200

    comment caching middleware

--- server/db.js
+++ server/db.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose')
 const config = require('../config')
 
 exports.connect = () => {
-  return mongoose.connect(config.MONGODB_URI, error => {
-    if (error) console.error('Could not connect to mongodb...💩 💩')
-  })
+  return mongoose.connect(
+    config.MONGODB_URI,
+    error => {
+      if (error)
+        console.error('Could not connect to mongodb...💩 💩', error.message)
+    }
+  )
 }
 
 exports.mongooseDisconnect = () => {
