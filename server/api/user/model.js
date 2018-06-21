6230a120bd640e749a8c3eb334141c9095ec0c54
Kévin <kevin.lemairecolombel@gmail.com>
2018-06-21 15:13:58 +0200

    add email unique true

--- server/api/user/model.js
+++ server/api/user/model.js
@@ -224,14 +224,6 @@ const UserSchema = new mongoose.Schema({
 //   next();
 // });
 
-UserSchema.post('save', function(error, doc, next) {
-  if (error.name === 'MongoError' && error.code === 11000) {
-    next(new Error('email must be unique'))
-  } else {
-    next(error)
-  }
-})
-
 const errorMessages = {
   MissingPasswordError: 'Mot de passe manquant',
   MissingUsernameError: 'Email manquant',
